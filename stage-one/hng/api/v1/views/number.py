32e2567dce12679781b851ca2fb5be99f9872400
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2025-02-04 12:14:28 +0100

    hotfix number.py

--- stage-one/hng/api/v1/views/number.py
+++ stage-one/hng/api/v1/views/number.py
@@ -1,25 +1,4 @@
-#!/usr/bin/python3
-from flask import Flask, render_template, make_response, jsonify
-from flask_cors import CORS
-from .views import app_views
 
-
-app = Flask(__name__)
-app.config['JSONIFY_PRETTPRINT_REGULAR'] = True
-app.register_blueprint(app_views)
-cors = CORS(app, resourCes={r"/*": {"origins":"*"}})
-
-
-@app.errorhandler(404)
-def not_found(error):
-    "404"
-    return make_response(jsonify({'error': "Not found"}), 404)
-
-if __name__ == "__main__":
-    host ='0.0.0.0'
-    port = '5000'
-    app.run(host=host, port=port, thread=True)
-(venv) ubuntu@1893-web-01:~/hng/api/v1$ cat views/number.py
 #!/usr/bin/python3
 """ Objects that handle all default RestFul API actions for Users """
 from . import app_views
